7c5d7791977c677f5846d8227ae4d945c23eabe4
Mitch Cravens <mitch.cravens@galvanize.com>
2018-11-05 12:02:55 -0700

    init

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // block vs inline elements
+// <p></p>, <div></div>, <article></article> - block - takes up its own line on the screen
+// <span></span>, <input />, <img> - inline - stays within the SAME line on the screen.
 
 // html forms - https://learn-2.galvanize.com/cohorts/714/blocks/63/content_files/html/subtrees/Forms.md
 // - what is a web form?
@@ -9,4 +11,4 @@
 // select
 // button type="submit"
 // useful form attributes
-
+// What happens when we submit a web form?
