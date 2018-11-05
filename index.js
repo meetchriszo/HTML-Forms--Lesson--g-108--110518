37b4f3f74ffdf38fdce23a05cb257708653ed428
Mitch Cravens <mitch.cravens@galvanize.com>
2018-11-05 09:57:50 -0700

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+// block vs inline elements
+
+// html forms - https://learn-2.galvanize.com/cohorts/714/blocks/63/content_files/html/subtrees/Forms.md
+// - what is a web form?
+// - forms and control tags
+// - the input tag and its types
+// radios and checkboxes
+// labels - id, for
+// select
+// button type="submit"
+// useful form attributes
+
